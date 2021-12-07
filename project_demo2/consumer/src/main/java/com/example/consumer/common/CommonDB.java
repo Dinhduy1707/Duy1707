8d4ccc877b79354fc9609d3fb24604c3b9507406
Dinhduy1707 <72538044+Dinhduy1707@users.noreply.github.com>
2021-12-07 14:22:58 +0700

    Delete project_demo2 directory

--- project_demo2/consumer/src/main/java/com/example/consumer/common/CommonDB.java
+++
@@ -1,8 +0,0 @@
-package com.example.consumer.common;
-
-public class CommonDB {
-    public final static int MAX_POOL_SIZE=30;
-    public final static String DATABASE_URL="jdbc:mysql://localhost:3306/test ";
-    public final static String DATABASE_USERNAME="root";
-    public final static String DATABASE_DRIVER_CLASS_NAME="com.mysql.cj.jdbc.Driver";
-}
--- project_demo2/producer/src/main/java/com/example/producer/service/RabbitService.java
+++
@@ -1,8 +0,0 @@
-package com.example.producer.service;
-
-import com.example.producer.dto.PaymentDTO;
-import com.example.producer.dto.ResultDTO;
-
-public interface RabbitService {
-    ResultDTO sendDataPayment(PaymentDTO paymentDTO);
-}
