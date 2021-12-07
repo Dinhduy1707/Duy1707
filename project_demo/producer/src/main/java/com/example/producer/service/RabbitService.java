152108a9c9c75ca11651adcd3f792e6de970e2c7
Dinhduy1707 <72538044+Dinhduy1707@users.noreply.github.com>
2021-12-07 14:42:52 +0700

    Delete project_demo directory

--- project_demo/producer/src/main/java/com/example/producer/service/RabbitService.java
+++
@@ -1,9 +0,0 @@
-package com.example.producer.service;
-
-import com.example.producer.dto.PaymentDTO;
-import com.example.producer.dto.ResponseDTO;
-import com.example.producer.dto.ResponsePartDTO;
-
-public interface RabbitService {
-    ResponsePartDTO sendDataPayment(PaymentDTO paymentDTO);
-}
