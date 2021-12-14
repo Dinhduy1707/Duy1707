9b9fc6a0facd6891658dc268584813cfd2436696
Dinhduy1707 <72538044+Dinhduy1707@users.noreply.github.com>
2021-12-14 13:16:39 +0700

    Delete project_demo directory

--- project_demo/consumer/src/main/java/com/example/consumer/repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.consumer.repository;
-
-import com.example.consumer.entity.Payment;
-
-import java.sql.SQLException;
-
-public interface PaymentRepository {
-    Boolean save(Payment payment) ;
-}
