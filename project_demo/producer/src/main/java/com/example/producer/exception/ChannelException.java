69dc1c6bf9b0fa65b6669ce22f6a88eb1197c378
Dinhduy1707 <72538044+Dinhduy1707@users.noreply.github.com>
2021-12-16 09:51:14 +0700

    Delete project_demo directory

--- project_demo/producer/src/main/java/com/example/producer/exception/ChannelException.java
+++
@@ -1,7 +0,0 @@
-package com.example.producer.exception;
-
-public class ChannelException extends RuntimeException {
-    public ChannelException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
